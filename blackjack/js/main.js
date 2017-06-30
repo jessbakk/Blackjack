cf23606ee93a5f945f7b68897ae484090b3a466d
Jess Bakkers <jessica.bakkers@gmail.com>
2017-06-29 22:36:20 -0700

    blackjack game push

---
+++ blackjack/js/main.js
@@ -0,0 +1,30 @@
+/*----- constants -----*/
+
+var dealerCards = document.querySelectorAll('.dealerCards')
+var playerCards = document.querySelector('.playerCards')
+
+var deck = [
+    'dA',
+    'd01',
+    'd02',
+    'd03',
+    'd04',
+    'd05'
+]
+
+
+/*----- app's state (variables) -----*/
+
+
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/
+
+
