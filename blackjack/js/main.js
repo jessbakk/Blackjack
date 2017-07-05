5dc3d42799b90797cf833bb5c603b039ce47aa0a
Jess Bakkers <jessica.bakkers@gmail.com>
2017-07-05 16:54:20 -0700

    wed commit with new changes

--- blackjack/js/main.js
+++ blackjack/js/main.js
@@ -1,88 +1,116 @@
 $(function() {
 
-var cardId;
-var cardValue;
 
-var bankroll = 1000;
-var currentBankroll;
+
+var bankroll = 1000; //balance when game starts for the first time
+var currentBankroll; //utilize local storage to maintain 
 
 var currentBet = [];
 var totalBet;
 
+var cardValue;
+
 var playersHand = [];
-var playersTotal;
+var playersTotal = 0;
 
 var dealersHand = [];
 var dealersTotal;
 
-$('#hit').attr('disabled','disabled');
-$('#stand').attr('disabled','disabled');
-$('#deal').attr('disabled','disabled');
+var hiddenCard;
+
+
 
 
 $('h1').css({color: "red", fontWeight: "bold", fontFamily: "Syncopate", fontSize: "75px", textAlign: "center"});
 
+$('p').css({fontFamily: "Syncopate", fontWeight: "bold", fontSize: "15px", margin: '0 auto'});
+
+render();
+
 $('#bet5').on('click', function() {
     currentBet.push(5);
     addBet();
-    $('#deal').attr('disabled',false);
+     $('#deal').attr('disabled', false);
 });
 
 $('#bet10').on('click', function() {
     currentBet.push(10);
-    addBet();
-    $('#deal').attr('disabled',false);
+    addBet(); 
+         $('#deal').attr('disabled', false);
 });
 
 $('#bet25').on('click', function() {
     currentBet.push(25);
     addBet();
-    $('#deal').attr('disabled',false);
+         $('#deal').attr('disabled', false);
 });
 
 $('#deal').on('click', function() {
     subBankroll();
     $('#bankroll').html(`Bankroll: $` + currentBankroll);
-    $('#bet5').attr('disabled', true);
-    $('#bet10').attr('disabled', true);
-    $('#bet25').attr('disabled', true);
-    randomCard();
-    playersHand.push(cardValue);
-    $("#player-card1").addClass(cardId);
+    $('.bet').attr('disabled', true);
+
+    playersHand.push(randomCard());
+    console.log("players first card " + playersHand[0].value);
+    $("#player-card1").addClass(playersHand[0].id);
         $("#player-card1").removeClass('back-blue');
-    randomCard();
-    dealersHand.push(cardValue);
-    $("#dealer-card1").addClass(cardId);
+
+    dealersHand.push(randomCard());
+    console.log("dealers first card " + dealersHand[0].value);
+    $("#dealer-card1").addClass(dealersHand[0].id);
         $("#dealer-card1").removeClass('back-red');
-    randomCard();
-    playersHand.push(cardValue);
-    $("#player-card2").addClass(cardId);
+
+    playersHand.push(randomCard());
+    console.log("players second card " + playersHand[1].value);
+    // console.log(playersHand);
+    $("#player-card2").addClass(playersHand[1].id);
         $("#player-card2").removeClass('back-blue');
     $('#deal').attr('disabled', true);
-    randomCard();
-    dealersHand.push(cardValue);
+
+    dealersHand.push(randomCard());
+    console.log("dealers first card " + dealersHand[1].value);
+    hiddenCard = dealersHand[1].id;
+    console.log(hiddenCard);
+
     addHand();
+
     checkBjWinner();
+    $('#hit').attr('disabled', false);
+    $('#stand').attr('disabled', false);
+    $('#playersTotal').html(`Players Hand: ` + playersTotal);
+    $('#currentBet').html(`Current Bet: $` + totalBet);
 });
 
+
+
 $('#hit').on('click', function() {
-    randomCard();
-    playersHand.push(cardValue);
+    playersHand.push(randomCard());
     addHand();
+    for (var i = 2; i < playersHand.length; i++) {
+        var newCard = 
+        (`<div class="playerCards card ${playersHand[i].id}"></div>`); 
+    }
+     $('#playersHand').append(newCard);
     if(playersTotal > 21) {
         console.log("BUST");
-        $("#dealer-card2").addClass(cardId);
+        $("#dealer-card2").addClass(hiddenCard);
         $("#dealer-card2").removeClass('back-red');
     }
+
 });
+
+
 
 $('#stand').on('click', function() {
     $('#hit').attr('disabled', true);
+    $("#dealer-card2").addClass(hiddenCard);
+        $("#dealer-card2").removeClass('back-red');
+    // playOutDealerHand();
     if(dealersTotal < 17) {
         randomCard();
-        dealersHand.push(cardValue);
+        dealersHand.push(randomCard());
         addHand();
-        console.log(cardId);
+        // console.log();
         $('#stand').trigger('click');
     } else if(dealersTotal > 21) {
         console.log("DEALER BUSTS");
@@ -96,13 +124,12 @@ $('#stand').on('click', function() {
 });
 
 
-
 function addBet() {
     var betSum = currentBet.reduce(function(sum, value) {
         return sum + value;
     }, 0); 
     totalBet = betSum;
-        console.log("total bet: " + betSum); 
+        // console.log("total bet: " + betSum); 
 }
 
 function subBankroll() {
@@ -110,38 +137,53 @@ function subBankroll() {
 }
 
 function randomCard() {
-    var cardIdx = Math.floor((Math.random() * deck.length));
-    cardId = deck[cardIdx].id;
-    cardValue = deck[cardIdx].value;
+    var deckIdx = Math.floor((Math.random() * deck.length));
+    // console.log(deck[deckIdx]);
+    return deck[deckIdx];
 }
 
 function addHand() {
-    var playerSum = playersHand.reduce(function(sum, value) {
-        return sum + value;
-    }, 0); 
-    playersTotal = playerSum;
+    console.log('JON', playersHand);
+    for (var i = 0; i < playersHand.length; i++) {
+        var value = playersHand[i].value;
+        console.log(value)
+        playersTotal += value;
         console.log("players total: " + playersTotal);
-    var dealerSum = dealersHand.reduce(function(sum, value) {
-        return sum + value;
-    }, 0); 
-    dealersTotal = dealerSum;
-        console.log("dealers total: " + dealersTotal);
+    }
+
+return playersTotal;
+        
+        // console.log("dealers total: " + dealersTotal);
+
 }
 
 function checkBjWinner() {
     if (playersTotal === 21 && dealersTotal === 21) {
         console.log("Push");
-        $("#dealer-card2").addClass(cardId);
+        $("#dealer-card2").addClass();
         $("#dealer-card2").removeClass('back-red');
     } else if(playersTotal === 21) {
         console.log("BLACKJACK!");
     } else if (dealersTotal === 21) {
         console.log("Dealer has Blackjack");
-        $("#dealer-card2").addClass(cardId);
+        $("#dealer-card2").addClass();
         $("#dealer-card2").removeClass('back-red');
     } else if (playersTotal < 21) {
         $('#hit').attr('disabled', false);
         $('#stand').attr('disabled', false);
     } 
 }
+
+
+function render() {
+    $('#hit').attr('disabled','disabled');
+    $('#stand').attr('disabled','disabled');
+    $('#deal').attr('disabled','disabled');
+    
+}
+
+
+
+
+
 });
